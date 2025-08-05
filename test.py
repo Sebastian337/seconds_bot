05450e62acd14014ba5e5da1519494baf0636c0d
Sebastian337 <aleshka_demidov@mail.ru>
2025-08-05 14:28:01 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-import os
-from dotenv import load_dotenv
+from decouple import config
 import ptbot
 from pytimeparse import parse
 
-TG_TOKEN = None
-TG_CHAT_ID = None
-bot = None
+
+TG_TOKEN = config('TG_TOKEN')
+TG_CHAT_ID = config('TG_CHAT_ID')
+
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
@@ -16,38 +16,35 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-def handle_message(chat_id, text):
+def handle_message(bot, chat_id, text):
     seconds = parse(text)
     if seconds is None:
         bot.send_message(chat_id, "5s")
     message_id = bot.send_message(chat_id, f"Таймер запущен на {seconds} секунд\n{render_progressbar(seconds, 0)}")
     bot.create_countdown(
         seconds,
-        notify_progress,
-        chat_id=chat_id,
-        message_id=message_id,
-        total=seconds
+        lambda secs_left: notify_progress(bot, secs_left, chat_id, message_id, seconds)
     )
-    bot.create_timer(seconds, notify, chat_id=chat_id)
+    
+    bot.create_timer(
+        seconds,
+        lambda: notify(bot, chat_id)
+    )
 
 
-def notify_progress(secs_left, chat_id, message_id, total):
+def notify_progress(bot, secs_left, chat_id, message_id, total):
     progress = total - secs_left
     progressbar = render_progressbar(total, progress)
     bot.update_message(chat_id, message_id, f"Осталось: {secs_left} секунд\n{progressbar}")
 
 
-def notify(chat_id):
-    bot.send_message(chat_id, "Время вышло") 
+def notify(bot, chat_id):
+    bot.send_message(chat_id, "Время вышло")
 
 
 def main():
-    global TG_TOKEN, TG_CHAT_ID, bot
-    load_dotenv()
-    TG_TOKEN = os.getenv('TG_TOKEN')
-    TG_CHAT_ID = os.getenv('TG_CHAT_ID')
-    bot = ptbot.Bot(TG_TOKEN)
-    bot.reply_on_message(handle_message)
+    bot = ptbot.Bot(config('TG_TOKEN'))
+    bot.reply_on_message(lambda chat_id, text: handle_message(bot, chat_id, text))
     bot.run_bot()
 
 
